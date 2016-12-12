97cd8115d3d6f26d05e63b59988d8865fbc058bd
JohnSimerlink <johnsimerlink@gmail.com>
2016-12-12 17:14:17 -0500

    partition WIP

--- 22-Sorting/partition.c
+++ 22-Sorting/partition.c
@@ -4,15 +4,17 @@
 void partition(const int* array, int start, int end);
 int main(){
   int array[] = {4,6,1,8,2,12,15,2};
-  //randomly choose one of the elements to be the partition
-  //** arrays;
   partition(array, 0, (int) (sizeof(array) / sizeof(array[0]) - 1));
 
 }
 
 void partition(const int* array, int start, int end){
+  int r, left, right; 
   srand(time(NULL));
-  int r = rand() % (end - start) + start;
-  printf(array[r]);
+  r = rand() % (end - start) + start;
+  printf("The value is %i",array[r]);
 
+  left = start;
+  right = end;
+  
 }
