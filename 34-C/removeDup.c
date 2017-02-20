0518187314ea3daa4e440d3ead135d71df77baa8
JohnSimerlink <johnsimerlink@gmail.com>
2017-02-20 21:40:27 +0000

    removeDup boilerplate/start

--- 34-C/removeDup.c
+++ 34-C/removeDup.c
@@ -1,5 +1,31 @@
-void prepend(Node n, Node list);
-typedef struct Nodes {
-  int value;
-  Node next;
-} Node;
+/*
+ * * C program to create a linked list and display the element in the list
+ * */
+#include <stdio.h>
+#include <malloc.h>
+#include <stdlib.h>
+
+void main(){
+
+  struct node {
+    int value;
+    struct node *next;
+  };
+  typedef struct node NODE;
+
+  NODE *head, *first, *temp = 0;// []what happens if I remove the =0? // does that temp = 0 statement assign a mem location for mem? [] look in assembly code with that removed and with it added // Wait we assigned the address of temp equal to 0. . 
+  int count = 0;
+  int choice = 1;
+  first = 0;
+
+  while (choice) {
+    head = (NODE *) malloc(sizeof(NODE));
+    printf("Enter the data item\n");
+    scanf("%d", &head-> value);
+    if (first != 0){
+      temp->ptr = head;
+      temp = head;
+    } else {
+      first = temp = head;
+    }
+    fflush(stdin);
