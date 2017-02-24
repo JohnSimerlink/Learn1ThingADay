3ea5c415075a36710c96edf72c582802cc6d14dd
JohnSimerlink <johnsimerlink@gmail.com>
2017-02-24 01:38:45 +0000

    header file

---
+++ 34-C/linkedlist.c
@@ -0,0 +1,58 @@
+/*
+ * * C program to create a linked list and display the element in the list
+ * */
+#include <stdio.h>
+#include <malloc.h>
+#include <stdlib.h>
+#include "linkedlist.h"
+
+LINKEDLIST * init(int nodeSize, void* comparator, void* printNode);
+void insert(NODE * head, int val);
+NODE * deleteIndex(NODE * head, int index);
+void removeDuplicates(NODE * head);
+void print(NODE* head);
+
+  struct node {
+    void * obj;
+    struct node *next;
+  };
+  typedef struct node NODE;
+  struct linkedList {
+    NODE * head;
+    int size; /*numBytes*/
+    int length;
+    void * comparatorFunction;
+    void * printNodeFunction;
+  }
+  typedef struct linkedList LINKEDLIST;
+
+
+LINKEDLIST * init(int nodeSize, void* comparator, void* printNode){
+  LINKEDLIST * list = malloc(sizeof(LINKEDLIST));
+  list->nodeSize = nodeSize;
+  list->comparator = comparatorFunction;
+  list->printNodeFunction = printNode;
+  list->head = NULL;
+  list->length = 0;
+  return list;
+}
+int length(LINKEDLIST * list){
+  return list->length;
+}
+void insert(LINKEDLIST * list, void * val){ /*Will work if HEAD is NULL*/
+  NODE * newNode = malloc(sizeof(NODE));
+  newNode->obj = val;
+  newNode->next = null;
+  NODE * tail = list->head+=sizeof(NODE)*(length-1);
+  tail->next = newNode;
+  list->length++;
+ }
+NODE * deleteIndex(NODE * head, int index){
+}
+
+void removeDuplicates(NODE * head){
+
+}
+
+void print(NODE* head){
+}
--- 34-C/linkedlist.h
+++ 34-C/linkedlist.h
@@ -1,57 +1,20 @@
-/*
- * * C program to create a linked list and display the element in the list
- * */
-#include <stdio.h>
-#include <malloc.h>
-#include <stdlib.h>
+struct node {
+  void * obj;
+  struct node *next;
+};
+typedef struct node NODE;
+struct linkedList {
+  NODE * head;
+  int size; /*numBytes*/
+  int length;
+  void * comparatorFunction;
+  void * printNodeFunction;
+}
+typedef struct linkedList LINKEDLIST;
 
 LINKEDLIST * init(int nodeSize, void* comparator, void* printNode);
+int length(LINKEDLIST * list){
 void insert(NODE * head, int val);
 NODE * deleteIndex(NODE * head, int index);
 void removeDuplicates(NODE * head);
 void print(NODE* head);
-
-  struct node {
-    void * obj;
-    struct node *next;
-  };
-  typedef struct node NODE;
-  struct linkedList {
-    NODE * head;
-    int size; /*numBytes*/
-    int length;
-    void * comparatorFunction;
-    void * printNodeFunction;
-  }
-  typedef struct linkedList LINKEDLIST;
-
-
-LINKEDLIST * init(int nodeSize, void* comparator, void* printNode){
-  LINKEDLIST * list = malloc(sizeof(LINKEDLIST));
-  list->nodeSize = nodeSize;
-  list->comparator = comparatorFunction;
-  list->printNodeFunction = printNode;
-  list->head = NULL;
-  list->length = 0;
-  return list;
-}
-int length(LINKEDLIST * list){
-  return list->length;
-}
-void insert(LINKEDLIST * list, void * val){ /*Will work if HEAD is NULL*/
-  NODE * newNode = malloc(sizeof(NODE));
-  newNode->obj = val;
-  newNode->next = null;
-  NODE * tail = list->head+=sizeof(NODE)*(length-1);
-  tail->next = newNode;
-  list->length++;
- }
-NODE * deleteIndex(NODE * head, int index){
-}
-
-void removeDuplicates(NODE * head){
-
-}
-
-void print(NODE* head){
-}
