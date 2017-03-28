a0505acb092cca2de2c471b8d6fb12305361eae8
JohnSimerlink <johnsimerlink@gmail.com>
2017-03-28 21:27:04 +0000

    minor changes

--- 36-AngularJS/firstapp/src/app/app.js
+++ 36-AngularJS/firstapp/src/app/app.js
@@ -1,24 +1,25 @@
-//this will be app.js
-import angular from 'angularjs'
-import style from '../style/app.css'
+import angular from 'angular';
+
+import '../style/app.css';
+
 let app = () => {
   return {
-    template: require('./app.html')
-    , controller: 'AppCtrl',
+    template: require('./app.html'),
+    controller: 'AppCtrl',
     controllerAs: 'app'
   }
-}
+};
 
 class AppCtrl {
   constructor() {
-    this.url = 'http://simer.link'
+    this.url = 'https://github.com/preboot/angular-webpack';
   }
 }
 
-const MODULE_NAME = 'app'
+const MODULE_NAME = 'app';
 
 angular.module(MODULE_NAME, [])
   .directive('app', app)
-  .controller('AppCtrl', AppCtrl)
+  .controller('AppCtrl', AppCtrl);
 
-export default MODULE_NAME
+export default MODULE_NAME;
--- 36-AngularJS/firstapp/src/tests.webpack.js
+++ 36-AngularJS/firstapp/src/tests.webpack.js
@@ -1,11 +1,10 @@
-//This file is an entry points for angular tests
-// avoids some weird issues when using webpack + angular
+// This file is an entry point for angular tests
+// Avoids some weird issues when using webpack + angular.
 
-import 'angular'
-import 'angular-mocks/angular-mocks'
+import 'angular';
+import 'angular-mocks/angular-mocks';
 
-const context = require.context('./app', true, /\.js$/)
+const context = require.context('./app', true, /\.js$/);
 
-context.keys().forEach(context) //WAIT: is context a hashmap or function??
-
+context.keys().forEach(context);
 
