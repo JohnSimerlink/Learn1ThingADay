b082379d15b7b43308a5290f566bddf5f15645f8
JohnSimerlink <johnsimerlink@gmail.com>
2017-03-31 16:29:40 +0000

    link function between scope and attrs

--- 36-AngularJS/journaldevtut1/app.js
+++ 36-AngularJS/journaldevtut1/app.js
@@ -4,6 +4,10 @@ app.directive('myEmployee', function() {
   return {
     scope: {},
     restrict: 'E',
-    template: "<br><br> <input type='text' ng-model='someText'> {{someText}}",
+    template: "<h3> Employee Role {{role}} {{boss}}</h3>",
+    link: function(scope, element, attrs){
+      scope.role = attrs.role
+      scope.boss = attrs.boss
+    }
   }
 })
