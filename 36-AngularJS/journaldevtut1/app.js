614c650686de891326184a6a10397bc6524c5a31
JohnSimerlink <johnsimerlink@gmail.com>
2017-03-31 16:20:39 +0000

    isolated scope

--- 36-AngularJS/journaldevtut1/app.js
+++ 36-AngularJS/journaldevtut1/app.js
@@ -2,6 +2,7 @@ var app = angular.module('mainApp', [])
 
 app.directive('myEmployee', function() {
   return {
+    scope: {},
     restrict: 'E',
     template: "<br><br> <input type='text' ng-model='someText'> {{someText}}",
   }
