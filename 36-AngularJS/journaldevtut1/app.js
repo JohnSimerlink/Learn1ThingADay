0fb59799513b2df204832085081e695150470b3f
JohnSimerlink <johnsimerlink@gmail.com>
2017-03-31 16:35:56 +0000

    isolated scope attribute binding

--- 36-AngularJS/journaldevtut1/app.js
+++ 36-AngularJS/journaldevtut1/app.js
@@ -2,12 +2,11 @@ var app = angular.module('mainApp', [])
 
 app.directive('myEmployee', function() {
   return {
-    scope: {},
+    scope: {
+      role: '@',
+      boss: '@'
+    },
     restrict: 'E',
     template: "<h3> Employee Role {{role}} {{boss}}</h3>",
-    link: function(scope, element, attrs){
-      scope.role = attrs.role
-      scope.boss = attrs.boss
-    }
   }
 })
