9836530e896b0efa704e5fc28ea5a1e97ff9e037
JohnSimerlink <johnsimerlink@gmail.com>
2017-04-01 15:31:49 +0000

    difference between ng-model and scope attribute binding

--- 36-AngularJS/journaldevtut1/app.js
+++ 36-AngularJS/journaldevtut1/app.js
@@ -4,9 +4,16 @@ app.directive('myEmployee', function() {
   return {
     scope: {
       role: '@',
-      boss: '@'
+      boss: '@',
+      name: '@'
     },
     restrict: 'E',
-    template: "<h3> Employee Role {{role}} {{boss}}</h3>",
+    template: "<h3> Employee Profile</h3> Role {{role}} {{boss}}</h3>" +
+      "<p> Role: {{role}}</p>" +
+      "<p> Name: {{name}}</p>" +
+      "<p> Boss: {{boss}}</p>" +
+      "<input name='role' ng-model='role' type='text'>" +
+      "<input name='name' ng-model='name' type='name'>" +
+      "<input name='boss' ng-model='boss' type='boss'>"
   }
 })
